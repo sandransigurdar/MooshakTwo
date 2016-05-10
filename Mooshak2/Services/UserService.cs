35d425209c2c11365b84e629741c398f27fe3a7e
DavidHaukur <davidhawk@visir.is>
2016-05-10 11:48:48 +0000

    create directory for assignments doesnt work

--- Mooshak2/Services/UserService.cs
+++ Mooshak2/Services/UserService.cs
@@ -60,6 +60,10 @@ namespace Mooshak2.Services
 
             if (user == 1)
             {
+                /*string path = "~/StudentAssignments/";
+                string coreDirectory = System.IO.Path.Combine(path, userName);
+                System.IO.Directory.CreateDirectory(coreDirectory);*/
+                
 
                 Student newStudent = new Student();
 
@@ -70,8 +74,8 @@ namespace Mooshak2.Services
                 newStudent.password = password;
                 newStudent.role = 1;
 
-                string coreDirectory = "~/StudentAssignments/";
-                System.IO.Directory.CreateDirectory(coreDirectory + name);
+               
+                
 
                 int courseId = 0;
 
