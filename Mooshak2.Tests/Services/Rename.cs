292fbf5f754f3bfea491c3e294b9c2a39d393844
sandransigurdar <sandrasigurdar@hotmail.com>
2016-05-10 17:01:09 +0000

    bla

--- Mooshak2.Tests/Services/Rename.cs
+++ Mooshak2.Tests/Services/Rename.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Mooshak2.Tests.Services
 {
     [TestClass]
-    public class UnitTest1
+    public class Rename
     {
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange:
+
+            // Act:
+
+            // Assert:
         }
     }
 }
+
+/*    Sýnidæmi um leap year, ættum að gera amk 4 test fyrir það fall)
+    [TestMethod]
+    public void TestIsLeapYearModulus400()
+    {
+        const int year = 2000;
+
+        var result = IsLeapYear(year);
+
+        Assert.IsTrue(result);
+    }
+
+    (Ættum svo að gera annað test fyrir Modulus100 sem er ekki fyrir 400, o.s.frv...)*/
